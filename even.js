6d72c97bd85ecc4cff0a1e213a13ac88c83df41a
marijkevanhell <jukevanhell@gmail.com>
2023-01-25 02:13:55 +0000

    playing with isEven function

---
+++ even.js
@@ -0,0 +1,23 @@
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+//example 1
+
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+//example 2
+
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+console.log(isEven(10));
+console.log(isEven(11));
+//exmple 3
