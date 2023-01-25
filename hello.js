47fed3c83630234905b2cae8e00fb4a61e8d74ee
marijkevanhell <jukevanhell@gmail.com>
2023-01-25 02:06:20 +0000

    testing functions

---
+++ hello.js
@@ -0,0 +1,14 @@
+const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+sayHello();
+//example 1
+
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Marijke")
+sayHello("John")
+sayHello("Jane")
